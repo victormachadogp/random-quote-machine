daaa7b454cc12dfd46687cf8e10075cdcac205fb
Nathan Torquato <ntorquato@voice.com>
2021-08-23 18:56:55 -0300

    feat: fetch random quote from API

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log('ahhahahahah')
+// #8: When the #new-quote button is clicked,
+// my quote machine should fetch a new quote
+// and display it in the #text element.
+const newQuoteButton = document.querySelector('#new-quote')
+newQuoteButton.addEventListener('click', fetchRandomQuote)
+
+async function fetchRandomQuote() {
+  const response = await fetch('https://api.quotable.io/random')
+  const quote = await response.json()
+  console.log(quote)
+}
