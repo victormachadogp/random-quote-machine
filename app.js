505640a30a196656c44003c475a8ae2850e1cecb
Nathan Torquato <ntorquato@voice.com>
2021-08-23 19:10:24 -0300

    feat: render quote and author

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
-// #8: When the #new-quote button is clicked,
-// my quote machine should fetch a new quote
-// and display it in the #text element.
 const newQuoteButton = document.querySelector('#new-quote')
 newQuoteButton.addEventListener('click', fetchRandomQuote)
 
 async function fetchRandomQuote() {
   const response = await fetch('https://api.quotable.io/random')
-  const quote = await response.json()
-  console.log(quote)
+  const quoteObject = await response.json()
+  renderQuote(quoteObject)
+  renderAuthor(quoteObject)
+}
+
+function renderQuote(quoteObject) {
+  const textElement = document.querySelector('#text')
+  textElement.textContent = quoteObject.content
+}
+
+function renderAuthor(quoteObject) {
+  const textElement = document.querySelector('#author')
+  textElement.textContent = `- ${quoteObject.author}`
 }
